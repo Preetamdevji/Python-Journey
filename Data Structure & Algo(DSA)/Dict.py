921fea36f3df15f1e2ac558f2f34579f4508cdef
Preetam <preetam.devji123@gmail.com>
2023-07-10 18:00:09 +0500

    Added two files of DSA<<<

---
+++ Data Structure & Algo(DSA)/Dict.py
@@ -0,0 +1,13 @@
+dict1 = {'mango': 100, 'apple': 450, 'strawberry': 550, 'banana': 150}
+print(dict1)
+
+ky = dict1.keys()
+val = dict1.values()
+print(dict1)
+print(ky)
+print(val)
+
+dict1['mango'] = 200
+
+print(val)
+print(dict1)
---
+++ Data Structure & Algo(DSA)/set.py
@@ -0,0 +1,6 @@
+set = {1 , 2 , 3 , 54 , 1 ,'preetam', 2,'preetam'}
+print(set)
+
+
+set.update([996,'8+2i'])   
+print(set)
